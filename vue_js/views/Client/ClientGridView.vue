08d61621ae6b206bdd8f343632487bc18a8af6ad
Vincent Moreau <vmoreau@proginov.com>
2025-05-23 15:11:12 +0200

    feat: ajustements JS

--- vue_js/views/Client/ClientGridView.vue
+++ vue_js/views/Client/ClientGridView.vue
@@ -1,19 +1,7 @@
-<template>
-  <div class="client-grid-view-container pa-4">
-    <h1 class="text-h5 mb-4">Client Management</h1>
-    <ClientGrid />
-  </div>
-</template>
-
 <script setup lang="ts">
 import ClientGrid from '@/components/Client/ClientGrid.vue'; // Adjusted path
 </script>
 
-<style scoped>
-.client-grid-view-container {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-}
-/* Add any view-specific styles here */
-</style>
+<template>
+  <ClientGrid />
+</template>
--- vue_js/views/Environment/EnvironmentGridView.vue
+++ vue_js/views/Environment/EnvironmentGridView.vue
@@ -1,19 +1,9 @@
-<template>
-  <div class="environment-grid-view-container pa-4">
-    <h1 class="text-h5 mb-4">Environment Management</h1>
-    <EnvironmentGrid />
-  </div>
-</template>
-
 <script setup lang="ts">
 import EnvironmentGrid from '@/components/Environment/EnvironmentGrid.vue'; // Adjusted path
 </script>
 
-<style scoped>
-.environment-grid-view-container {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-}
-/* Add any view-specific styles here */
-</style>
+<template>
+  <EnvironmentGrid />  
+</template>
+
+
